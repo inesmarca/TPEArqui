fcb7d452ef15367f5d419a8b91bb6b26ee656ff9
inesmarca <imarcarian@itba.edu.ar>
2020-05-24 14:48:20 -0300

    testeo de procesador

--- Kernel/asm/test.asm
+++ Kernel/asm/test.asm
@@ -1,4 +1,6 @@
 GLOBAL test
+GLOBAL test2
+extern printf
 section .text
 
 test:
@@ -25,9 +27,28 @@ test:
     pop rbp
 	ret
 
+test2:
+    push rbp
+	mov rbp,rsp
+
+    mov rax, 0
+	cpuid
+    mov rdi, fmt
+	mov rsi, string
+	mov [rsi], ebx
+	mov [rsi+4], edx
+	mov [rsi+8], ecx
+	call printf
+
+    mov rsp,rbp
+    pop rbp
+	ret
+
 section .data
 msg db "HOLA BUENAS NOCHES",10
 len equ $-msg
+fmt db "%s", 10, 0
 
 section .bss
 str: resb 15
+string: resb 12
